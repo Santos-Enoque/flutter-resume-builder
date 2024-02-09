6cdcb85ec7769886703b2e163cd2d43c728b2841
Santos Safrao <santossafrao@Santoss-MacBook-Pro.local>
2024-02-09 14:36:55 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/santosenoque/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.santosenoque.resumestudio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:resumestudio/app/app.dart';
+import 'package:resumestudio/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
